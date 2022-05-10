5ef61833922c17cfb8897dd260885a0ff4f5e627
Redhead <86776617+alin2025@users.noreply.github.com>
2022-05-10 22:45:59 +0300

    Create step3_spark_to_parquet.py

---
+++ HDFS Ecosystem Code/Airflow/step3_spark_to_parquet.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+import os
+import time
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+with DAG(dag_id='dags_3_hdfs_parquet_dag', schedule_interval='@once', start_date=datetime(2021, 12, 17), catchup=False) as dag:
+
+    # Task 1
+    dummy_task = DummyOperator(task_id='dags_3_hdfs_parquet_dag')
+
+    # Task 2
+    bash_task1 = BashOperator(task_id='dags_3_hdfs_parquet_dag_start',
+	bash_command='python /tmp/pycharm_project_764/venv/step3_spark_to_parquet.py')
+
+    dummy_task >> bash_task1
